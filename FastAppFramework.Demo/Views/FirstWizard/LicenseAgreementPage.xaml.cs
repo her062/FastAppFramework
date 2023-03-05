b61ac7f5867cccddf5cbfa96300db06056991d32
yusuke.yoshimoto <itis.wonderful.that.iam.alive@gmail.com>
2023-03-05 23:21:38 +0900

    Refactoring Divide each pages to xaml in FirstWizardFrame

---
+++ FastAppFramework.Demo/Views/FirstWizard/LicenseAgreementPage.xaml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace FastAppFramework.Demo.Views
+{
+    public partial class LicenseAgreementPage : UserControl
+    {
+        public LicenseAgreementPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
